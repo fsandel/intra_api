f26cdccd5033732baa383f3d57b2232900cb08fa
Florian Sandel <108098144+fsandel@users.noreply.github.com>
2023-07-08 10:07:01 +0000

    cleaned up a little

--- main.py
+++ main.py
@@ -12,17 +12,13 @@ SECRET = secret
 API_URL = "https://api.intra.42.fr"
 
 def get_user_data(username):
-    # Create an OAuth2 session using the client credentials
     client = BackendApplicationClient(client_id=UID)
     oauth = OAuth2Session(client=client)
     token = oauth.fetch_token(token_url=f"{API_URL}/oauth/token", client_id=UID, client_secret=SECRET)
 
-    # Perform a GET request to /v2/users/{username}
     response = oauth.get(f"{API_URL}/v2/users/{username}")
 
-    # Check the response status
     if response.status_code == 200:
-        # Parse the response content as JSON
         data = response.json()
         return data
     else:
@@ -30,45 +26,34 @@ def get_user_data(username):
         return None
 
 def display_profile_picture(image_url, width=None, height=None):
-    # Retrieve the image data
     image_response = requests.get(image_url, stream=True)
-    
-    # Check the image response status
+
     if image_response.status_code == 200:
-        # Open the image with PIL
         image = Image.open(image_response.raw)
-        
-        # Save the resized image locally
+
         with open("profile_image.jpg", "wb") as f:
             image.save(f, "JPEG")
-        
-        # Display the image in the terminal using imgcat
+
         subprocess.run(["imgcat", "profile_image.jpg"])
         
-        # Delete the image file
         os.remove("profile_image.jpg")
     else:
         print("Failed to retrieve the image.")
 
 def main():
-    # Retrieve the username from the command-line argument
     if len(sys.argv) < 2:
         print("Please provide a username as an argument.")
         sys.exit(1)
 
     username = sys.argv[1]
     
-    # Get user data
     user_data = get_user_data(username)
-    if user_data:
-        # Access the medium version of the image URL
-        image_url_medium = user_data["image"]["versions"]["small"]
 
-        
-        # Display the profile picture with resizing
-        display_profile_picture(image_url_medium)
-        
-        # Display the current location
+    if user_data:
+        image_url = user_data["image"]["versions"]["small"]
+
+        display_profile_picture(image_url)
+
         print(user_data["location"])
 
 if __name__ == "__main__":
